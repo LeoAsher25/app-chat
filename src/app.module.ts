81c57e18ad2e25a212ea8aadecb48965f5333792
LeoAsher25 <leoasher2507@gmail.com>
2022-10-22 00:20:59 +0700

    feat: handle register with validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { UsersModule } from './users/users.module';
 
 @Module({
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
-import * as authMiddleware from './auth.middleware';
+import AuthMiddleware from './auth.middleware';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
@@ -19,22 +19,14 @@ import { JwtStrategy } from './jwt.strategy';
       signOptions: { expiresIn: '3600s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, AuthMiddleware],
   exports: [AuthService],
 })
-// export class AuthModule implements NestModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer.apply(authMiddleware.register).forRoutes('/auth/register');
-//   }
-// }
-// import { Module } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-// import { UsersModule } from '../users/users.module';
-// import { PassportModule } from '@nestjs/passport';
-// import { LocalStrategy } from './local.strategy';
-
-// @Module({
-//   imports: [UsersModule, PassportModule],
-//   providers: [AuthService, LocalStrategy],
-// })
-export class AuthModule {}
+export class AuthModule {
+  constructor(private readonly authMiddleware: AuthMiddleware) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('auth/register');
+    // consumer.apply(AuthMiddleware).forRoutes('auth/register');
+    // consumer.apply(AuthController)
+  }
+}
